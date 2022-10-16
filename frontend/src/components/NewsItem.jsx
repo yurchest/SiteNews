52ec92e183a8e2ffafc34af61b0684dc8ebbe041
Yuriy PC <yurchest@gmail.com>
2022-10-16 22:14:49 +0300

    test1

--- frontend/src/components/NewsItem.jsx
+++ frontend/src/components/NewsItem.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const NewsItem = (props) => {
-    return (
-        <div className="news">
-            <div className="news__content">
-                <strong>{props.news.id}. {props.news.content}</strong>
-                <div>
-                    <a href={props.news.url}>{props.news.url}</a>
-                </div>
-                <div>
-                    {new Date(props.news.time_update).toLocaleString()}
-                </div>
-            </div>
+  return (
+    <div className="news">
+      <div className="news__content">
+        <strong>
+          {props.news.id}. {props.news.content}
+        </strong>
+        <div>
+          <a href={props.news.url}>{props.news.url}</a>
         </div>
-    );
+        <div>{new Date(props.news.time_update).toLocaleString()}</div>
+      </div>
+    </div>
+  );
 };
 
 export default NewsItem;
--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const NotFound = () => {
-    return (
-        <div>
-            <h1>Page Not Found </h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Page Not Found </h1>
+    </div>
+  );
 };
 
 export default NotFound;
