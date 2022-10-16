2fa3fe61c26c57f6e09fec779e426546d74a914e
Yuriy <yurchest@email.com>
2022-10-16 22:36:01 +0300

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,21 @@
 import Navbar from "./components/UI/navbar/Navbar";
-import "./styles/main.css"
-import Home from "./components/Home"
-import {
-  Routes,
-  Route,
-} from "react-router-dom"
+import "./styles/main.css";
+import Home from "./components/Home";
+import { Routes, Route } from "react-router-dom";
 import NotFound from "./components/NotFound";
 
 function App() {
-
-
-  return (
-   <>
-      <Navbar/>
-      <div>
-        <Routes>  
-          <Route path="/home" element={<Home/>}/>
-            <Route path = "*" element={<NotFound/>}/>
-        </Routes>
-      </div>
-   </>  
-  );
+    return (
+        <>
+            <Navbar />
+            <div>
+                <Routes>
+                    <Route path="/home" element={<Home />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </div>
+        </>
+    );
 }
 
 export default App;
